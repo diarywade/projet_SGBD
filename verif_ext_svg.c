c324d9e8518d7ac239b6430cbced2f93fc5c4e1c
diarywade <46231673+diarywade@users.noreply.github.com>
2019-01-02 22:04:41 +0000

    Update verif_ext_svg.c

--- verif_ext_svg.c
+++ verif_ext_svg.c
@@ -7,7 +7,7 @@ int verifsvg(char* argument[] ,int tail)
 if (tail==7){
 		int tail_Fi_sor;
 		tail_Fi_sor=strlen(argument[6]);
-		if ((argument[6][tail_Fi_sor-4]=='.') && (argument[6][tail_Fi_sor-3]=='s') && (argument[6][ttail_Fi_sor-2]=='v') && (argument[6][tail_Fi_sor-1]=='g'))
+		if ((argument[6][tail_Fi_sor-4]=='.') && (argument[6][tail_Fi_sor-3]=='s') && (argument[6][tail_Fi_sor-2]=='v') && (argument[6][tail_Fi_sor-1]=='g'))
 
 			return 0;
 		else
